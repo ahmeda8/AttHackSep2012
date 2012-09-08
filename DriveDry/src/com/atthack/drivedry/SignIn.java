33784173e93c40aec8e78cf0bb096ee7bd023cfc
Sam Brotherton <sbrother@gmail.com>
2012-09-08 13:32:45 -0700

    work on signup

--- DriveDry/src/com/atthack/drivedry/SignIn.java
+++ DriveDry/src/com/atthack/drivedry/SignIn.java
@@ -1,16 +1,18 @@
 package com.atthack.drivedry;
 
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
 
 public class SignIn extends Activity {
 	
 	Button SigninBtn;
+	EditText usernamefield;
+	EditText emailfield;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,9 @@ public class SignIn extends Activity {
 
         SigninBtn =(Button) findViewById(R.id.button1);
         SigninBtn.setOnClickListener(signinclick);
+        
+        usernamefield = (EditText) findViewById(R.id.editText1);
+        emailfield = (EditText) findViewById(R.id.editText2);
 	}
 	
 	View.OnClickListener signinclick = new View.OnClickListener() {
@@ -26,8 +31,14 @@ public class SignIn extends Activity {
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 
-			Log.e("signin","This is the signin click");
+			String usertext = usernamefield.getText().toString();
+			String emailtext = emailfield.getText().toString();
 			
+		    String urlsstring = "http://www.driveaward.com/api/register;name=" + usertext + ";email=" + emailtext;
+		    Log.e("signin",urlsstring);
+		    String s = HttpFetch.Get(urlsstring);
+		    Log.e("signin","Result is"+s);
+		   
 		}
 	};
 }
