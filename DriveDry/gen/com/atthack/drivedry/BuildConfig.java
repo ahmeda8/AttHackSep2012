425bfb199a7ea409a8fe7fa6222f67bd1a5ae83f
Abrar Ahmed <ahmeda5011@hotmail.com>
2012-09-08 00:13:24 -0700

    init commit

---
+++ DriveDry/gen/com/atthack/drivedry/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.atthack.drivedry;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ DriveDry/src/com/atthack/drivedry/SmsReceiver.java
@@ -0,0 +1,27 @@
+package com.atthack.drivedry;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		Bundle pudsBundle = arg1.getExtras();
+		Object[] pdus = (Object[]) pudsBundle.get("pdus");
+		SmsMessage messages = SmsMessage.createFromPdu((byte[])pdus[0]);
+		String destinationAddress = messages.getOriginatingAddress();
+		SmsManager sms = SmsManager.getDefault();
+		String text = "Hi Honey I am driving right now.";
+		Log.e("sms", "message received");
+		sms.sendTextMessage(destinationAddress, null, text, null, null);
+		
+	}
+	
+}
